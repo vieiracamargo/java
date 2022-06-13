f4995827495a7adc9a0e7501868e651934f99d55
vieiracamargo <brunovieiracamargo@gmail.com>
2022-06-13 17:14:39 -0300

    first commit

---
+++ src/TestaVariaveis.java
@@ -0,0 +1,10 @@
+public class TestaVariaveis {
+
+    public static void main(String [] args){
+        System.out.println("Olá, Mundo");
+
+        int idade = 37;
+
+        System.out.println("a idade é: " + idade + " anos.");
+    }
+}
---
+++ src/TetaCaracteres.java
@@ -0,0 +1,16 @@
+public class TetaCaracteres {
+    public static void main (String [] args){
+        char letra = 'a';
+
+        System.out.println(letra);
+
+        char valor = 65;
+        System.out.println(valor);
+
+        valor = (char) (valor + 1);
+        System.out.println(valor);
+
+        String palavra = "Alura cursos online de tecnologia";
+        System.out.println(palavra);
+    }
+}
